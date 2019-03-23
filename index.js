5a207c77dd5b66f0f88058597c33276687c348f1
Bragi Bergthorsson <bragur@gmail.com>
2019-03-23 14:52:09 +0000

    Adding simple stuff

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
-require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use('/', (req, res) => {
   res.send({ name: 'Óli' });
